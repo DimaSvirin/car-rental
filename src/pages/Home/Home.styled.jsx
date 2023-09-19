840ff5164dbb9aedaf292bd31bff5b850e716014
DimaSvirin <dimasvirin@gmail.com>
2023-09-19 16:19:38 +0300

    update project

---
+++ src/pages/Home/Home.styled.jsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const BigTitle = styled.h1`
+    font-family: 'Manrope';
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 1.5;
+    justify-content: center;
+    display: flex;
+    margin-bottom: 20px;
+`
+export const Text = styled.p`
+    font-family: 'Manrope';
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 1.5;
+`
+export const Span = styled.span`
+    font-family: 'Manrope';
+    font-weight: 600;
+    font-size: 15px;
+    line-height: 1.5;
+    text-decoration: dashed;
+`
+export const List = styled.ul`
+    list-style: disc;
+    padding-left: 17px;
+`
+export const ListItemsTitle = styled.p`
+    font-family: 'Manrope';
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 1.5;
+`
+export const AboutUsTitle = styled.h2`
+    font-family: 'Manrope';
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 1.5;
+    margin-bottom: 10px;
+`
+export const ServicesTitle = styled.h3`
+    font-family: 'Manrope';
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 1.5;
+    margin-top: 30px;
+    margin-bottom: 10px;
+`
+export const AdvantageTitle = styled.h4`
+    font-family: 'Manrope';
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 1.5;
+    margin-top: 30px;
+    margin-bottom: 10px;
+`
+export const ContactTitle = styled.h5`
+    font-family: 'Manrope';
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 1.5;
+    margin-top: 30px;
+    margin-bottom: 10px;
+`
