c2993a3c73bb6cfc738930cedfb946f79027fe5b
DimaSvirin <dimasvirin@gmail.com>
2023-09-19 16:56:12 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from 'components/App';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "components/App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='car-rental-project'>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="car-rental">
     <App />
   </BrowserRouter>
 );
